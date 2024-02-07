5178bcabca68fde5ed0d7807aaa80abb3319ad28
GeekchanskiY <astrovskiy.04@gmail.com>
2024-02-07 12:20:56 +0300

    test

---
+++ articles.rb
@@ -0,0 +1,9 @@
+class Article
+
+end
+
+if $0 == __FILE__
+   puts "articles is being run directly."
+else
+    puts "articles is being required as a module."
+end
---
+++ main.rb
@@ -0,0 +1,4 @@
+require_relative "articles"
+
+# include Article
+
